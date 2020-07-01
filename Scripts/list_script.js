d5359cc78b38f35dd29ee2c5f9ecb3b45c15dfdb
Felix Freidman <felixfreidman@gmail.com>
2020-07-02 06:55:43 +0800

    JS доработал Всю верстку добил, сейчас буду соединять

--- Scripts/list_script.js
+++ Scripts/list_script.js
@@ -2,7 +2,69 @@ document.addEventListener('click', function(event) {
     let variable = event.target.dataset.id;
     if (variable != undefined) {
         document.getElementById(variable).parentNode.removeChild(document.getElementById(variable));
-
-
     }
+});
+
+const addButton = document.getElementById("addTask");
+const capture = document.querySelector(".capture");
+const unorderedList = document.getElementById("ulToHide");
+const addingForm = document.querySelector(".adding_form");
+const closingButton = document.querySelector(".closingButton");
+const addTaskButton = document.querySelector(".addTaskButton");
+const nameTask = document.getElementById("task_name");
+const dateTask = document.getElementById("task_date");
+const mark = document.querySelectorAll('input[type="radio"]');
+const readyMark = document.getElementById("readyMark");
+const notreadyMark = document.getElementById("notreadyMark");
+
+
+addButton.addEventListener('click', () => {
+    capture.classList.toggle('hide');
+    unorderedList.classList.toggle('hide');
+    addingForm.classList.toggle('hide');
+});
+
+closingButton.addEventListener('click', () => {
+    capture.classList.toggle('hide');
+    unorderedList.classList.toggle('hide');
+    addingForm.classList.toggle('hide');
+});
+
+addTaskButton.addEventListener("click", () => {
+    let newLiElement = document.createElement('li');
+    let nameOfTask = nameTask.value;
+    let dateOfTask = dateTask.value;
+    let statusOfTask = "";
+
+
+    if (readyMark.checked) {
+        statusOfTask = "ready";
+        messageStatus = "Выполнено";
+    } else if (notreadyMark.checked) {
+        statusOfTask = 'not_ready';
+        messageStatus = "Не выполнено";
+    } else {
+        statusOfTask = 'received_response';
+        messageStatus = "Получен ответ";
+    }
+    newLiElement.innerHTML = `
+        <div class="name general">${nameOfTask}</div>
+        <div class="date general">${dateOfTask}</div>
+        <div class="readiness">
+            <div class="${statusOfTask} mark">${messageStatus}</div>
+        </div>
+        <div class="change general"><a href="change_task.html"><span>Изменить</span></a></div>
+        <div>
+            <button data-id="first" class="delete">
+                <span class="cross first-line"></span>
+                <span class="cross second-line"></span>
+            </button>
+        </div>
+    `;
+    unorderedList.append(newLiElement);
+    nameTask.value = "";
+    dateTask.value = "";
+    capture.classList.toggle('hide');
+    unorderedList.classList.toggle('hide');
+    addingForm.classList.toggle('hide');
 });
